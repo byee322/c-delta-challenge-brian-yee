b70f9861c26cb7cbaf17f96329e982e9f829e46e
Brian Yee <byee322@gmail.com>
2018-11-08 22:27:16 -0800

    creative quality show.html.erb - normalized scores

--- app/controllers/creative_qualities_controller.rb
+++ app/controllers/creative_qualities_controller.rb
@@ -2,6 +2,14 @@ class CreativeQualitiesController < ApplicationController
   include ApplicationHelper
   def index
     @creative_qualities = CreativeQuality.all
+    @normalized_scores = {}
+
+    @creative_qualities.each do |quality|
+    	@normalized_scores[quality.id] = 0
+    end
+
+    normalize_scores
+    	    
     respond_to do |format|
       format.html
       format.json { render json: mock_creative_quality_scores }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,22 @@ module ApplicationHelper
         score: scores[i]
       )
     end
-
     data
   end
+
+  def normalize_scores
+    SurveyResponse.all.each do |survey|
+      survey.answers.all.each do |answer|
+        choice = answer.question_choice
+        @normalized_scores[choice.creative_quality_id] += choice.score
+      end
+    end
+
+    CreativeQuality.all.each do |quality|
+      total_max_score = quality.max_score * SurveyResponse.all.count
+      score = ((@normalized_scores[quality.id].to_f / total_max_score) * 100).round
+      @normalized_scores[quality.id] = score
+    end
+  end
+
 end
